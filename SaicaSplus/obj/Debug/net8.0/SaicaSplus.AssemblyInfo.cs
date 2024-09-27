5da5d3859442e07ece1e893745671577c2829e1b
selçuk çıraklı <cirakli@hotmail.com>
2024-09-27 16:55:37 +0300

    sql yetki ekleme

--- SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
+++ SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651c620ccd12950ac6ae77ba82025e0ffa747c1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

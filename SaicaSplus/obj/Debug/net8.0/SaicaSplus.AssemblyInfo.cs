ddec2dd75e8e103fd41f0c1a4c45ce1003dc8c94
Ayşegül Sever <aysesever.126@gmail.com>
2024-09-30 11:51:14 +0300

    1

--- SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
+++ SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
@@ -6,9 +6,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651c620ccd12950ac6ae77ba82025e0ffa747c1e")]
-
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0d4d72fe0517615fe5b02ef586dc968ca19027")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

fc57629796d80f7a045d387c77dfa2eddc6685a7
selçuk çıraklı <cirakli@hotmail.com>
2024-09-30 11:44:16 +0300

    a

--- SaicaSplus/Data/UserRepository.cs
+++ SaicaSplus/Data/UserRepository.cs
@@ -16,7 +16,7 @@ public class UserRepository
         using (var connection = new SqlConnection(_connectionString))
         {
             connection.Open();
-            var command = new SqlCommand("SELECT s_user_domain FROM s_user where aktif=13", connection);
+            var command = new SqlCommand("SELECT s_user_domain FROM s_user where aktif=15", connection);
             using (var reader = command.ExecuteReader())
             {
                 while (reader.Read())

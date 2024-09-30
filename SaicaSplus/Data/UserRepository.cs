01969a0327c5fdefb7beaef5752e0bc6bbd36f72
Ayşegül Sever <aysesever.126@gmail.com>
2024-09-30 15:35:39 +0300

    aaa

--- SaicaSplus/Data/UserRepository.cs
+++ SaicaSplus/Data/UserRepository.cs
@@ -17,7 +17,7 @@ public class UserRepository
         {
             connection.Open();
 
-            var command = new SqlCommand("SELECT s_user_domain FROM s_user where aktif=2", connection);
+            var command = new SqlCommand("SELECT s_user_domain FROM s_user where aktif=1", connection);
             
             using (var reader = command.ExecuteReader())
             {
--- SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
+++ SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff7f511991d42c52897030af2784459a2dc1128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000a98d3082db20fccc8055f6467f1ee922f59ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

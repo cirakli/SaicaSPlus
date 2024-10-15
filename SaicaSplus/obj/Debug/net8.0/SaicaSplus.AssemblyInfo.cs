de27a2136b88bffe89bcc63fc106c14d27f49eb7
selçuk çıraklı <cirakli@hotmail.com>
2024-10-15 13:51:55 +0300

    aaa

--- SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
+++ SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3420a24d3f3c41f2458f77d3250aba14f50150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b9ee0d3c9580c2b4f92c5e4e36f0b743d15b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

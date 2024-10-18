c200afc998022e2439d48353f40b300b704b3d2e
selçuk çıraklı <cirakli@hotmail.com>
2024-10-18 17:03:54 +0300

    dashboard_2

--- SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
+++ SaicaSplus/obj/Debug/net8.0/SaicaSplus.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ade709cdbda34e63922fc7e8f18e9f5dcd5a7a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa11df1ba2ce2705f1238c718ebd8702b7975fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaicaSplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
